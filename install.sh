7c3c8f2731c3a54ff068066c680ef940005e563e
Takuya Kitazawa <k.takuti@gmail.com>
2023-02-10 21:38:36 -0800

    Stop downloading mac-provisioning repo from install.sh
    because you already have the repo locally when install.sh is executed.

--- install.sh
+++ install.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-TMP_DIR="${HOME}/tmp"
-
-ARCHIVE="${TMP_DIR}/master.zip"
-SRC_DIR="${TMP_DIR}/mac-provisioning-master"
-
-mkdir -p ${TMP_DIR}
-
-curl -LSfs -o ${ARCHIVE} https://github.com/takuti/mac-provisioning/archive/master.zip
-unzip -oq ${ARCHIVE} -d ${TMP_DIR}
-
-cd ${SRC_DIR}
-
 if [ -z $(which brew) ]; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
@@ -19,5 +7,3 @@ fi
 brew install ansible --HEAD
 ansible-galaxy collection install community.general
 ansible-playbook -i localhost playbook.yml
-
-rm -rf ${TMP_DIR}
